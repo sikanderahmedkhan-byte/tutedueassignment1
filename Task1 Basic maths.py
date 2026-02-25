eb7f61ec8a0ec5e1d27e3383483cf1fb4bf7a1fd
sikanderahmedkhan-byte <sikanderahmed.khan@gmail.com>
2026-02-26 04:03:55 +0530

    Add files via upload

---
+++ Task1 Basic maths.py
@@ -0,0 +1,27 @@
+num1 = input ("enter first number: ")
+num2 = input ("enter second number: ")
+"""
+a=input("addition: (num1 + num2)")
+b=input("Subtraction: (num1 - num2)")
+c=input("multiplication: (num1 * num2)")
+d=input("division: (num1 / num2)")
+print (num1,num2)
+"""
+print (int(num1) + int(num2))
+print (int(num1) - int(num2))
+print (int(num1) * int(num2))
+print (int(num1) / int(num2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Task2 Greeting.py
@@ -0,0 +1,10 @@
+f= input ("Enter first Name: ")
+l= input ("Enter Last Name: ")
+
+print (f,l)
+g= input ("welcome to learning python")
+
+
+
+
+
